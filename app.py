b6eeb15d17d7f4b55813d581c7371e079b1fe0cf
Gdula <da.gdula@gmail.com>
2019-05-30 02:35:31 +0200

    Create class app and gui

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ from gui import *
 
 
 class App(object):
+
+    def __init__(self):
+        self.gui = Gui()
+
     def run(self):
         gui = Gui()
         root = Tk()
         label = Label(root, text="Figury")
-        label.pack()
+        #label.pack()
 
-        gui.draw_buttons(root)
+        #gui.draw_buttons(root)
+        gui.menu(root)
+        choice = "triangle"
+        #app.figure_factory(choice, root)
 
         # gui.canvas(root)
         root.mainloop()
--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from figures import *
+from app import *
 
 
 class Gui(object):
@@ -7,56 +7,124 @@ class Gui(object):
     def __init__(self):
         self.WIDTH = 500
         self.HEIGHT = 500
+        self.CENTER = Point(self.WIDTH // 2, self.HEIGHT // 2)
 
     def canvas(self, root):
         canvas = Canvas(root, width=self.WIDTH, height=self.HEIGHT)
 
-        CENTER = Point(self.WIDTH // 2, self.HEIGHT // 2)
-
-        # p = RegularPolygon(5, 400, *CENTER, "red", "blue")
-        po = RegularOctagon(400, *CENTER, "red", "blue")
-        #for point in p.points:
-            #print(point.x, point.y)
+        p = RegularPolygon(5, 400, *self.CENTER, "red", "blue")
+        po = RegularOctagon(400, *self.CENTER, "red", "blue")
 
         #po.draw(canvas)
         #t = IsoscelesTriangle("white", "black", 2, 1)
         #t.draw(canvas)
         #t2 = EquilateralTriangle(400, *CENTER, "red", "blue")
         #t2.draw(canvas)
-        c = Parallelogram("red", "black", Point(20, 20), Point(300, 30), Point(400, 100), Point(50, 200))
+        #canvas.delete(All)
+        c = Parallelogram("red", "black", Point(10, 20), Point(300, 30), Point(400, 100), Point(50, 200))
         c.draw(canvas)
         canvas.pack()
 
-    def draw_buttons(self, root):
+    def menu(self, root):
+        canvas = Canvas(root, width=self.WIDTH, height=self.HEIGHT)
+        enter_button = Button(root, text="Trójkąt", command=lambda: self.draw_triangle(root, canvas))
+        enter_button.pack()
+        canvas.pack()
+
+    def draw_triangle(self, root, canvas):
+
         top_frame = Frame(root)
         top_frame.pack()
         bottom_frame = Frame(root)
         bottom_frame.pack(side=BOTTOM)
 
-        button_1 = Button(top_frame, text="Trójkąt", fg="green")
-        button_2 = Button(top_frame, text="Czworokąt", fg="green")
-        button_3 = Button(top_frame, text="Wielokąt foremny", fg="green")
-        button_4 = Button(top_frame, text="Pentagon", fg="green")
-        button_5 = Button(top_frame, text="Hexagon", fg="green")
-        button_6 = Button(top_frame, text="Oktagon", fg="green")
-        button_7 = Button(top_frame, text="Trójkąt równoramienny", fg="green")
-        button_8 = Button(top_frame, text="Trójkąt równoboczny", fg="green")
-        button_9 = Button(top_frame, text="Równoległobok", fg="green")
-        button_10 = Button(top_frame, text="Deltoid", fg="green")
-        button_11 = Button(top_frame, text="Romb", fg="green")
-        button_12 = Button(top_frame, text="Kwadrat", fg="green")
+        text = Label(root, text="Trójkąt")
+        text.pack()
 
-        button_1.pack()
-        button_2.pack()
-        button_3.pack()
-        button_4.pack()
-        button_5.pack()
-        button_6.pack()
-        button_7.pack()
-        button_8.pack()
-        button_9.pack()
-        button_10.pack()
-        button_11.pack()
-        button_12.pack()
+        fill_colour = StringVar()
+        entry = Entry(root, width=10, textvariable=fill_colour)
+        entry.pack()
+
+        outline_colour = StringVar()
+        entry = Entry(root, width=10, textvariable=outline_colour)
+        entry.pack()
+
+        a_label = IntVar()
+        entry = Entry(root, width=10, textvariable=a_label)
+        entry.pack()
+
+        b_label = IntVar()
+        entry = Entry(root, width=10, textvariable=b_label)
+        entry.pack()
+
+        c_label = IntVar()
+        entry = Entry(root, width=10, textvariable=c_label)
+        entry.pack()
 
 
+        enter_button = Button(root, text="Enter",command=lambda: self.get_triangle(fill_colour.get(),
+                            outline_colour.get(), a_label.get(), b_label.get(),c_label.get(), canvas))
+        enter_button.pack()
+
+    def draw_convex_quadrilateral(self, root, canvas):
+        text = Label(root, text="Czworokąt")
+        text.pack()
+
+        fill_colour = StringVar()
+        entry = Entry(root, width=10, textvariable=fill_colour)
+        entry.pack()
+
+        outline_colour = StringVar()
+        entry = Entry(root, width=10, textvariable=outline_colour)
+        entry.pack()
+
+        a_label_x = IntVar()
+        entry = Entry(root, width=10, textvariable=a_label_x)
+        entry.pack()
+
+        a_label_y = IntVar()
+        entry = Entry(root, width=10, textvariable=a_label_y)
+        entry.pack()
+
+        b_label_x = IntVar()
+        entry = Entry(root, width=10, textvariable=b_label_x)
+        entry.pack()
+
+        b_label_y = IntVar()
+        entry = Entry(root, width=10, textvariable=b_label_y)
+        entry.pack()
+
+        c_label_x = IntVar()
+        entry = Entry(root, width=10, textvariable=c_label_x)
+        entry.pack()
+
+        c_label_y = IntVar()
+        entry = Entry(root, width=10, textvariable=c_label_y)
+        entry.pack()
+
+        d_label_x = IntVar()
+        entry = Entry(root, width=10, textvariable=d_label_x)
+        entry.pack()
+
+        d_label_y = IntVar()
+        entry = Entry(root, width=10, textvariable=d_label_y)
+        entry.pack()
+
+        enter_button = Button(root, text="Enter", command=lambda: self.get_convex_quarilateral(fill_colour.get(),
+        outline_colour.get(), Point(a_label_x.get(), a_label_y.get()), Point(b_label_x.get(), b_label_y.get()),
+        Point(c_label_x.get(), c_label_y.get()), Point(d_label_x.get(), d_label_y.get()), canvas))
+        enter_button.pack()
+
+    def get_triangle(self, fill_colour, outline_colour, a_label, b_label, c_label, canvas):
+        canvas.delete("all")
+        triangle = Triangle(fill_colour, outline_colour, a_label, b_label, c_label)
+        canvas.create_text(150, 10, text=("Obwód", triangle.perimeter()))
+        canvas.create_text(150, 30, text=("Pole ", triangle.area()))
+        triangle.draw(canvas)
+
+    def get_convex_quarilateral(self, fill_colour, outline_colour, A, B, C, D, canvas):
+        canvas.delete("all")
+        convex_quarilateral = ConvexQuadrilateral(fill_colour, outline_colour, A, B, C, D)
+        canvas.create_text(150, 10, text=("Obwód", convex_quarilateral.perimeter()))
+        canvas.create_text(150, 30, text=("Pole ", convex_quarilateral.area()))
+        convex_quarilateral.draw(canvas)
